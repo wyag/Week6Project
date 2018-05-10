39b60f980b895a010ac55cc065547d2a05e08eff
Isaac <isaacjgonzalez25@gmail.com>
2018-05-10 09:43:42 -0600

    Initial Commit

---
+++ GameProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameProject
+//
+//  Created by Isaac Gonzalez on 5/10/18.
+//  Copyright © 2018 Isaac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
